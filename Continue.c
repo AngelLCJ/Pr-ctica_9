0fe9e090e790613db5f505a922ab3e813da20858
AngelLCJ <54157013+AngelLCJ@users.noreply.github.com>
2019-10-17 00:14:36 -0500

    Update Continue.c

--- Continue.c
+++ Continue.c
@@ -5,11 +5,19 @@
 #define LIMITE 5 
  
 int main (){ 
-        int enteroContador = 1; 
-        int enteroNumero = 0;         int enteroSuma = 0;         while (enteroContador <= LIMITE){                 printf("Ingrese número par %d:", enteroContador);                 scanf("%d",&enteroNumero); 
- 
-                if (enteroNumero%2 != 0){                         printf("El número insertado no es par.\n");                         continue;                 } 
- 
-                enteroSuma += enteroNumero;                 enteroContador++;         }        printf("La suma de los números es: %d\n", enteroSuma); 
- 
-        return 0; } 
+ int enteroContador = 1; 
+ int enteroNumero = 0;      
+ int enteroSuma = 0;    
+ while (enteroContador <= LIMITE){        
+  printf("Ingrese número par %d:", enteroContador);      
+  scanf("%d",&enteroNumero); 
+                if (enteroNumero%2 != 0){         
+                 printf("El número insertado no es par.\n");   
+                 continue;             
+                } 
+  enteroSuma += enteroNumero;       
+  enteroContador++;      
+ }        
+ printf("La suma de los números es: %d\n", enteroSuma); 
+       return 0; 
+} 
